f42a540ed8a39848e2208150c7bc79a87e8ca220
Mbithikakui <mbithikakui159@gmail.com>
2026-06-04 01:18:02 +0300

    updated files

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -185,7 +185,7 @@ MPESA_CONSUMER_KEY         = os.getenv('MPESA_CONSUMER_KEY',          '')
 MPESA_CONSUMER_SECRET      = os.getenv('MPESA_CONSUMER_SECRET',       '')
 MPESA_SHORTCODE            = os.getenv('MPESA_SHORTCODE',             '')
 MPESA_PASSKEY              = os.getenv('MPESA_PASSKEY',               '')
-MPESA_ENVIRONMENT          = os.getenv('MPESA_ENVIRONMENT',           'sandbox')
+MPESA_ENVIRONMENT          = os.getenv('MPESA_ENVIRONMENT',           'production')  # 'sandbox' or 'production'
 MPESA_CALLBACK_URL         = os.getenv('MPESA_CALLBACK_URL',          '')
 
 # B2C Configurations
